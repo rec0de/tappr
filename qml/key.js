3e38f83cedd6778a53bd978055981c7cc1af2376
rec0de <mail@rec0de.net>
2014-04-29 21:08:56 +0200

    Changed key.js comment

--- qml/key.js
+++ qml/key.js
@@ -1,6 +1,7 @@
 // This file contains the secret key used to authentificate scores
 // The key in the github version is invalid in order to prevent cheating
 
+
 function get(){
     return 'c2ae92b0c0ae555d6ef67c10f312f356aec3d84667d';
 }
