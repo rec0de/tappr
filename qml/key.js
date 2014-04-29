5b0e2136ec7318ca729a5becea158b6c715b9d85
rec0de <mail@rec0de.net>
2014-04-29 21:05:21 +0200

    Added RPMS & changed key.js comment

--- qml/key.js
+++ qml/key.js
@@ -1,5 +1,5 @@
 // This file contains the secret key used to authentificate scores
-// The key in the github version has been replaced with 0 to prevent cheating
+// The key in the github version is invalid in order to prevent cheating
 
 function get(){
     return 'c2ae92b0c0ae555d6ef67c10f312f356aec3d84667d';
